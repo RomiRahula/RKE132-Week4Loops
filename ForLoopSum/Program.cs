969e331c8d11b5510eb69a53893094904d2a05c5
Romi <romi.rahula@tktk.ee>
2023-03-04 00:46:49 +0200

    add week4loops code

---
+++ ForLoopSum/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+
+int sum = 0;
+
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"i ={i}");
+    Console.WriteLine($"Current Total: {sum}");
+    sum = sum + i;
+}
+Console.WriteLine($" Final Total {sum}");
